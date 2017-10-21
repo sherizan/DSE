0faac386cd1cdd1b05c9435925f36af41cf1b83f
sherizan <sherizan@gmail.com>
2017-10-21 15:55:31 +0800

    Update site name

--- Front End/dashboard/index.php
+++ Front End/dashboard/index.php
@@ -11,7 +11,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.10.2/sweetalert2.all.min.js"></script>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">SIM Search</a>
+  <a class="navbar-brand" href="#">SIM DSE</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>

c88c9dc2e5a33380f795bf729547ea7fbde381cf
sherizan <sherizan.sheikh@grabtaxi.com>
2017-10-31 20:12:37 +0800

    Display IP address

--- Frontend/index.php
+++ Frontend/index.php
@@ -331,6 +331,7 @@ use App\SQLiteConnection;
               echo "<li>Simply combine them like &ldquo;lazy dog&rdquo; &#92; charlie</li>";
               echo "</ul>";
               echo "</div>";
+              echo "Go to " . $_SERVER['REMOTE_ADDR'] . " to view from your own device.";
             
             }
 
